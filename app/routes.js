a091f4713b6c10316674a770996c7b320f3b3be6
Michael Gearon <50828707+mgearon-ch@users.noreply.github.com>
2022-01-27 21:00:02 +0000

    Changes

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post('/no-error', function (req, res) {
+    res.redirect('overview?upload=succesful')
+})
+
+router.get('/overview', function (req, res) {
+    // Render the confirm company page
+    res.render('overview', {
+      // To use the company data on that page use the following
+      upload: req.query.upload,
+    })
+  })
 
 module.exports = router
